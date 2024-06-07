4032435048c1971b0a8d6dd6556a9128bdade2b5
jjackchoi <jackchoi0325@gmail.com>
2024-06-08 00:48:13 +0900

    백) todo 삭제 구현

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -113,6 +114,37 @@ public class TodoController {
 		// (6) ResponseDTO를 리턴한다.
 		return ResponseEntity.ok().body(response);
 		
+	}
+	
+	// todo 삭제
+	@DeleteMapping
+	public ResponseEntity<?> deleteTodo(@RequestBody TodoDTO dto) {
+		try {
+			String temporaryUserId = "temporary-user"; // temporary user id
+			
+			// (1) TodoEntity로 변환한다.
+			TodoEntity entity = TodoDTO.toEntity(dto);
+			
+			// (2) 임시 사용자 아이디를 설정해 준다.
+			entity.setUserId(temporaryUserId);
+			
+			// (3) 서비스를 이용해 entity를 삭제한다.
+			List<TodoEntity> entities = service.delete(entity);
+			
+			// (4) 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다.
+			List<TodoDTO> dtos = entities.stream().map(TodoDTO :: new).collect(Collectors.toList());
+			
+			// (5) 변환된 TodoDTO 리스트를 이용해 ResponseDTO를 초기화한다.
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+			
+			// (6) ResponseDTO를 리턴한다.
+			return ResponseEntity.ok().body(response);
+		}catch(Exception e) {
+			// (7) 혹시 예외가 있는 경우 dto 대신 error에 메세지를 넣고 리턴한다.
+			String error = e.getMessage();
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+			return ResponseEntity.badRequest().body(response); 
+		}
 	}
 	
 }
